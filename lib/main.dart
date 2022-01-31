1d81cd8ed42766c7a2bae10962ba423ecc87b499
Tirth88663 <tirthp88663@gmail.com>
2022-01-31 17:46:50 +0530

    Base structure

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MedicineReminder());
+
+class MedicineReminder extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+        child: Container(),
+      ),
+    );
+  }
+}
